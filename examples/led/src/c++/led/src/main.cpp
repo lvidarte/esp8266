e690574df995e745240bbe8e14a0509a02a9bb00
Leo Vidarte <lvidarte@gmail.com>
2017-02-09 15:12:05 -0300

    Add c++ led code

---
+++ examples/led/src/c++/led/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define LED_PIN 16
+
+void setup ()
+{
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop ()
+{
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
