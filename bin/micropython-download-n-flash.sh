257ad325237c6b0df0734238d9347423a6ef50d5
Leo Vidarte <lvidarte@gmail.com>
2017-02-09 16:03:44 -0300

    Add bin scripts

--- bin/micropython-download-n-flash.sh
+++ bin/micropython-download-n-flash.sh
@@ -3,7 +3,11 @@
 PORT=${1:-/dev/ttyUSB0}
 FIRMWARE="http://micropython.org/resources/firmware/esp8266-20170108-v1.8.7.bin"
 
+if [ ! -f `basename $FIRMWARE` ]
+then
+    curl -O -J $FIRMWARE
+fi
+
 source esptool/env/bin/activate && \
-    curl -O -J $FIRMWARE && \
     esptool.py --port $PORT erase_flash && \
     esptool.py --port $PORT write_flash -fm dio -fs 32m 0x00000 `basename $FIRMWARE`
---
+++ bin/micropython-shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PORT=${1:-/dev/ttyUSB0}
+
+screen $PORT 115200
