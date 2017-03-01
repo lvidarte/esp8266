b4e2505595cd8eb44a6cb3d33f075e293a74a176
Leo Vidarte <lvidarte@gmail.com>
2017-03-01 17:38:51 -0300

    Add pullup help on code

--- examples/button/src/c++/button/src/main.cpp
+++ examples/button/src/c++/button/src/main.cpp
@@ -11,10 +11,20 @@ void setup ()
 
 void loop ()
 {
-  static int state = 0;
+  static uint8_t state;
 
+  // digitalRead() returns the current level of the pin,
+  // either 1 for a high logic level or 0 for a low logic level.
+  // Notice how the button is at a high level (value returns 1) when
+  // it's not pressed. This is because the pull-up resistor keeps the pin at
+  // a high level when it's not connected to ground through the button.
+  // When the button is pressed then the input pin connects to ground
+  // and reads a low level (value returns 0).
   state = digitalRead(D5);
 
+  // Remember that the internal led turn on
+  // when the pin is LOW
   digitalWrite(D0, state);
+
   delay(100);
 }
--- examples/button/src/python/main.py
+++ examples/button/src/python/main.py
@@ -5,8 +5,19 @@ led = Pin(16, Pin.OUT)
 button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 while True:
+
+    # The value function returns the current level of the pin,
+    # either 1 for a high logic level or 0 for a low logic level.
+    # Notice how the button is at a high level (value returns 1) when
+    # it's not pressed. This is because the pull-up resistor keeps the pin at
+    # a high level when it's not connected to ground through the button.
+    # When the button is pressed then the input pin connects to ground
+    # and reads a low level (value returns 0).
     if not button.value():
-        led.high()
-    else:
+        # Remember that the internal led turn on
+        # when the pin is LOW
         led.low()
-    sleep(1)
+    else:
+        led.high()
+
+    sleep(.1)
