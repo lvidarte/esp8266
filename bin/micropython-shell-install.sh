7803ae0251c2ad40aad880e98f74d342f1b57d66
Leo Vidarte <lvidarte@gmail.com>
2017-03-14 11:36:26 -0300

    Add micropython shell script installation

---
+++ bin/micropython-shell-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for DIR in `ls -1 examples`
+do
+    if [ -d "examples/$DIR/src/python" ]
+    then
+        if [ ! -d "examples/$DIR/src/python/bin" ]
+        then
+            mkdir "examples/$DIR/src/python/bin"
+        fi
+        ln -fs $(pwd)/bin/micropython-shell.sh examples/$DIR/src/python/bin/shell.sh
+    fi
+done
