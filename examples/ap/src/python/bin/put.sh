398254d26c539641ac6a73318d0f43477336601e
Leo Vidarte <lvidarte@gmail.com>
2017-04-04 12:59:09 -0300

    Remove unnecessary code from shell script

--- examples/ap/src/python/bin/put.sh
+++ examples/ap/src/python/bin/put.sh
@@ -8,11 +8,6 @@ fi
 PORT=${1:-/dev/ttyUSB0}
 echo "Using $PORT"
 
-if [ ! -f template.py ]; then
-    echo "template doesn't exist! creating..."
-    bin/minify.py > template.py
-fi
-
 for FILE in boot.py; do
     bin/ampy -p $PORT put $FILE
 done
